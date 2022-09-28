996942790442391293f4547773f9d15fdcf5031a
Bryan McCarthy <bryanmccarthy@Bryans-MacBook-Pro.local>
2022-09-28 13:15:22 +0100

    add edit color selector

--- lab1/problem2/app.js
+++ lab1/problem2/app.js
@@ -1,4 +1,4 @@
-const note = {
+ const note = {
 	noteText: document.querySelector('#note-input'),
 	color: document.querySelector('#color-selector'),
 }
@@ -8,16 +8,22 @@ const form = {
 }
 
 const notes = document.querySelector('.notes-container');
+const colors = ['blue', 'orange', 'green', 'pink'];
 
 function addNote() {
 	let text = note.noteText.value;
 	let noteBody = document.createElement('div');
+  let editColorSelector = document.createElement('select');
   let editBtn = document.createElement('button');
   let deleteBtn = document.createElement('button');
 
-	noteBody.classList.add('note');
+  noteBody.classList.add('note');
   noteBody.classList.add(note.color.value);
   noteBody.innerHTML = `<div class="note">${text}</div>`;
+
+  let colorOrder = getColorOrder(note.color.value); // Get the three colors not used
+  editColorSelector.classList.add('edit-color-selector')
+  editColorSelector.innerHTML = `<option value="${note.color.value}" selected>${note.color.value}</option><option value="${colorOrder[0]}">${colorOrder[0]}</option><option value="${colorOrder[1]}">${colorOrder[1]}</option><option value="${colorOrder[2]}">${colorOrder[2]}</option>`
 
   editBtn.classList.add('edit-note');
   editBtn.innerHTML = 'edit';
@@ -25,6 +31,7 @@ function addNote() {
   deleteBtn.classList.add('delete-note');
   deleteBtn.innerHTML = 'delete';
 
+  noteBody.appendChild(editColorSelector);
   noteBody.appendChild(editBtn)
   noteBody.appendChild(deleteBtn);
   notes.appendChild(noteBody);
@@ -32,8 +39,23 @@ function addNote() {
   note.noteText.value = '';
   note.noteText.focus();
 
+  editColorSelectorListener(editColorSelector);
   editButtonListener(editBtn);
   deleteButtonListener(deleteBtn);
+}
+
+function editColorSelectorListener(editColorSelector) {
+  editColorSelector.addEventListener('change', e => {
+    e.stopPropagation();
+    changeNoteColor(e, editColorSelector.value);
+  });
+}
+
+function changeNoteColor(e, color) {
+  let note = e.target.parentNode;
+  note.classList.remove('blue', 'orange', 'green', 'pink') // Remove existing color from classList
+  note.classList.add(color);
+  console.log(note)
 }
 
 function editButtonListener(editBtn) {
@@ -46,6 +68,9 @@ function editButtonListener(editBtn) {
 function editNote(e) {
   let note = e.target.parentNode;
   note.toggleAttribute('contenteditable');
+
+  console.log(note)
+
   let editBtnText = e.target;
   if (editBtnText.innerHTML === "edit") {
     editBtnText.innerHTML = "save";
@@ -71,4 +96,14 @@ if(form.addBtn) {
 	  e.preventDefault();  
 	   addNote();
 	})
+}
+
+function getColorOrder(currentColor) {
+  let colorOrder = []
+  for (let colorIdx in colors) {
+    if (colors[colorIdx] != currentColor) {
+      colorOrder.push(colors[colorIdx])
+    }
+  }
+  return colorOrder;
 }
