83343cdcea123a8c1d6cb58e1b2a97a7f35c57a5
Bryan McCarthy <bryanmccarthy@Bryans-MacBook-Pro.local>
2022-10-10 16:53:17 +0100

    comments

--- lab1/problem3/content.js
+++ lab1/problem3/content.js
@@ -1,4 +1,4 @@
-//array of images
+// Array of images
 let catsImages = [
     "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
     "https://e3.365dm.com/21/03/768x432/skynews-cats-missing-microchip_5315182.jpg?20210323142004",
@@ -7,6 +7,7 @@ let catsImages = [
     "https://lh3.googleusercontent.com/pw/AL9nZEXYJlrVkYoKIkpx07_3F4HOiTiOheaoaiRAcwrUg3C613-jkzEubJ3k8Z9fDjG5IfVqCzorphZ00vp6mIyB79GtCsoyV69xXe9cqrA0zglgrcvYhH2UP4cDR88WTm1AmuyCxQHAWCB5JzKD7eD94dtNZA=w690-h920-no"
 ];
 
+// Array of cat breeds
 let catBreeds = [
   "Devon Rex",
   "Abyssinian",
@@ -21,31 +22,32 @@ let catBreeds = [
 
 let catVideo = "https://www.youtube.com/watch?v=MUws5oXXYa8";
 
-//reverse through array of images
-//getting random image from the array we created before (we use math.floor and math.random to grab a random index in the array)
+// Reverse through array of images
+// Getting random image from the array we created before (we use math.floor and math.random to grab a random index in the array)
 const imgs = document.getElementsByTagName("img");
 for(let i = 0; i < imgs.length; i++) {
     const randomImg = Math.floor(Math.random() * catsImages.length);
     imgs[i].src = catsImages[randomImg];
 }
-//do the same for h1 elements
+
+// Do the same for h1 elements
 const headers = document.getElementsByTagName("h1");
 for (let i = 0; i < headers.length; i++) {
     headers[i].innerText = "Cats are awesome.";
 }
-//do the same for p elements
+// Do the same for p elements
 const p = document.getElementsByTagName("p");
 for (let i = 0; i < p.length; i++) {
     p[i].innerText = "This website is now about cats.";
 }
 
-//href link for every a element is changed to a cat video
+// Href link for every a element is changed to a cat video
 const a = document.getElementsByTagName("a");
 for (let i = 0; i < a.length; i++) {
     a[i].href = catVideo;
 }
 
-//button elements changed to say dont press me
+// Button elements changed to say dont press me
 const buttons = document.getElementsByTagName("button");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].innerText = "Don't press me";
@@ -53,10 +55,10 @@ for (let i = 0; i < buttons.length; i++) {
   buttonListener(buttons[i]);
 }
 
-//all elements 
+// All elements 
 const elements = document.getElementsByTagName("*");
 
-//button pressed clears the page and says cat
+// Button pressed clears the page and says cat in large font
 function buttonListener(button) {
   button.addEventListener('click', e => {
     e.stopPropagation();
@@ -69,7 +71,7 @@ function buttonListener(button) {
   });
 }
 
-//list elements show random cat breeds
+// List elements show random cat breeds
 const lists = document.getElementsByTagName("li");
 for (let i = 0; i < lists.length; i++) {
   const catBreed = Math.floor(Math.random() * catBreeds.length);
