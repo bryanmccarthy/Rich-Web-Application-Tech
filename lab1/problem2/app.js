a2f92fe5049444db55c1f2a97ecbcf59d77b0a7c
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-10-15 23:09:06 +0100

    fixes

--- lab1/problem2/app.js
+++ lab1/problem2/app.js
@@ -8,35 +8,32 @@ const form = {
 }
 
 const notes = document.querySelector('.notes-container');
-const colors = ['blue', 'orange', 'green', 'pink'];
 
 // Add note element with content and append edit and delete elements
 const addNote = () => {
-	let text = note.noteText.value;
-	let noteBody = document.createElement('div');
-  let editColorSelector = document.createElement('select');
-  let editBtn = document.createElement('button');
-  let deleteBtn = document.createElement('button');
+	const text = note.noteText.value;
+	const noteBody = document.createElement('div');
+  const editColorSelector = document.createElement('select');
+  const editBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
+  const otherColors = getOtherColors(note.color.value); // Get the three colors not used
 
   noteBody.classList.add('note');
   noteBody.classList.add(note.color.value);
   noteBody.innerHTML = `<div class="note">${text}</div>`;
+  editColorSelector.classList.add('edit-color-selector');
 
-  let colorOrder = getColorOrder(note.color.value); // Get the three colors not used
-  editColorSelector.classList.add('edit-color-selector')
-  editColorSelector.innerHTML = `<option value="${note.color.value}" selected>${note.color.value}</option><option value="${colorOrder[0]}">${colorOrder[0]}</option><option value="${colorOrder[1]}">${colorOrder[1]}</option><option value="${colorOrder[2]}">${colorOrder[2]}</option>`
-
+  // Add a color selector to each note with correct color ordering
+  editColorSelector.innerHTML = `<option value="${note.color.value}" selected>${note.color.value}</option><option value="${otherColors[0]}">${otherColors[0]}</option><option value="${otherColors[1]}">${otherColors[1]}</option><option value="${otherColors[2]}">${otherColors[2]}</option>`;
   editBtn.classList.add('edit-note');
   editBtn.innerHTML = 'edit';
-
   deleteBtn.classList.add('delete-note');
   deleteBtn.innerHTML = 'delete';
 
   noteBody.appendChild(editColorSelector);
-  noteBody.appendChild(editBtn)
+  noteBody.appendChild(editBtn);
   noteBody.appendChild(deleteBtn);
   notes.appendChild(noteBody);
-
   note.noteText.value = '';
   note.noteText.focus();
 
@@ -53,13 +50,6 @@ const editColorSelectorListener = (editColorSelector) => {
   });
 }
 
-// Remove any existing color from classList and add the new color
-const changeNoteColor = (e, color) => {
-  let note = e.target.parentNode;
-  note.classList.remove('blue', 'orange', 'green', 'pink') // Remove existing color from classList
-  note.classList.add(color);
-}
-
 // Edit button listener for editBtn that is appended to each note
 const editButtonListener = (editBtn) => {
   editBtn.addEventListener('click', e => {
@@ -70,15 +60,9 @@ const editButtonListener = (editBtn) => {
 
 // Edit note button toggles the note element to be editable
 const editNote = (e) => {
-  let note = e.target.parentNode;
+  const note = e.target.parentNode;
   note.toggleAttribute('contenteditable');
-
-  let editBtnText = e.target;
-  if (editBtnText.innerHTML === "edit") {
-    editBtnText.innerHTML = "save";
-  } else {
-    editBtnText.innerHTML = "edit";
-  }
+  const editBtnText = e.target.innerHTML === "edit" ? e.target.innerHTML = "save" : e.target.innerHTML = "edit";
 }
 
 // Delete button listener for deleteBtn that is appended to each note
@@ -95,21 +79,21 @@ const deleteNote = (e) => {
   note.parentNode.removeChild(note);
 }
 
-// Only create the addEventListener if the addBtn exists
-if(form.addBtn) {
-	form.addBtn.addEventListener('click', e => {
-	  e.preventDefault();  
-	   addNote();
-	})
+// Add button listener 
+form.addBtn.addEventListener('click', e => {
+  e.preventDefault();  
+  addNote();
+})
+
+// Remove any existing color from classList and add the new color
+const changeNoteColor = (e, color) => {
+  let note = e.target.parentNode;
+  note.classList.remove('blue', 'orange', 'green', 'pink'); // Remove any existing color from classList
+  note.classList.add(color);
 }
 
 // Get the colors that are not the current notes color
-const getColorOrder = (currentColor) => {
-  let colorOrder = []
-  for (let colorIdx in colors) {
-    if (colors[colorIdx] != currentColor) {
-      colorOrder.push(colors[colorIdx])
-    }
-  }
-  return colorOrder;
+const getOtherColors = (currentColor) => {
+  const colors = ['blue', 'orange', 'green', 'pink'];
+  return otherColors = colors.filter(color => color != currentColor);
 }
