17882ca2ced65ffce855de05e0dfe687a33fd441
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-11-15 15:46:04 +0000

    add button event for adding note

--- lab3/src/app.js
+++ lab3/src/app.js
@@ -1,10 +1,20 @@
+import { fromEvent } from 'rxjs';
+
 const note = {
 	noteText: document.querySelector('#note-input'),
 	color: document.querySelector('#color-selector'),
 }
 
 const form = {
-	addBtn: document.querySelector('#add-button'),
+	addNoteBtn: document.querySelector('#add-note'),
 }
 
 const notes = document.querySelector('.notes-container');
+
+const addNote = () => {
+  const noteText = note.noteText.value;
+  console.log(noteText);
+}
+
+const addNoteButton = fromEvent(form.addNoteBtn, 'click');
+addNoteButton.subscribe(() => addNote());
