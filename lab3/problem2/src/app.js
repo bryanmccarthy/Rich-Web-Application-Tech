3830b2883550f6b6854adc3f9ba54adc2298a8b7
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-11-19 15:52:55 +0000

    add function for countdown logic

--- lab3/problem2/src/app.js
+++ lab3/problem2/src/app.js
@@ -1,4 +1,4 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent, timer } from 'rxjs';
 
 const countdownButton = document.querySelector('.countdown-button');
 const hours = document.querySelector('.hours-input');
@@ -6,4 +6,15 @@ const minutes = document.querySelector('.minutes-input');
 const seconds = document.querySelector('.seconds-input');
 
 const countdownEvent = fromEvent(countdownButton, 'click');
-countdownEvent.subscribe((e) => console.log(`${hours.value}:${minutes.value}:${seconds.value}`));
+countdownEvent.subscribe((e) => countdownTimer(hours.value, minutes.value, seconds.value));
+
+const countdownTimer = (hours, minutes, seconds) => {
+  const totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+  timer(0, 1000).subscribe((t) => {
+    const totalSecondsRemaining = totalSeconds - t;
+    const remainingHours = Math.floor(totalSecondsRemaining / 3600);
+    const remainingMinutes = Math.floor((totalSecondsRemaining % 3600) / 60);
+    const remainingSeconds = totalSecondsRemaining % 60;
+    console.log(`${remainingHours}:${remainingMinutes}:${remainingSeconds}`);
+  });
+}
