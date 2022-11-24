89ffb5c2ee7b8d67a5c9fd32eaa4ac4091943c26
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-11-24 20:01:33 +0000

    add addChildNote function to Note class

--- lab3/problem3/src/app.js
+++ lab3/problem3/src/app.js
@@ -9,7 +9,16 @@ class Note {
   addNote() {
     console.log("note: ", this);
   }
+
+  addChildNote(note) {
+    this.children.push(note);
+    note.parent = this;
+    console.log(this);
+  }
 }
 
 const note = new Note("note");
 note.addNote();
+
+const childNote = new Note("child note");
+note.addChildNote(childNote);
