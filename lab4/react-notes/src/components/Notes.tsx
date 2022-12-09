2c3405a5ffef0cfd20926c611131b2be6712c725
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-09 14:58:33 +0000

    render test notes onclick

--- lab4/react-notes/src/components/Notes.tsx
+++ lab4/react-notes/src/components/Notes.tsx
@@ -1,9 +1,37 @@
 import "./Notes.css";
+import { useState } from "react";
+
+type Note = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 function Notes() {
+
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const addNote = () => {
+    const newNote = {
+      id: notes.length + 1,
+      title: "test title",
+      body: "test body",
+    };
+    setNotes([...notes, newNote]);
+  };
+
   return (
     <div className="Notes">
       <h1>Notes</h1>
+      <button onClick={addNote}>Add Note</button>
+      {
+        notes.map(note => (
+          <div key={note.id}>
+            <h2>{note.title}</h2>
+            <p>{note.body}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
