359aa8ad87b42d3d7f6fb3706fdf9f0a81a64296
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-09 20:14:00 +0000

    move add notes to app

--- lab4/react-notes/src/App.tsx
+++ lab4/react-notes/src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css';
 import Notes from './components/Notes';
+import { useState } from "react";
+
+type NoteType = {
+  id: number,
+  title: string,
+  body: string
+}
 
 function App() {
 
+  const [notes, setNotes] = useState<NoteType[]>([]);
+
+  const addNote = () => {
+    const newNote = {
+      id: notes.length + 1,
+      title: "",
+      body: "",
+    };
+    setNotes([...notes, newNote]);
+  };
+
   return (
     <div className="App">
-      <Notes />
+      <h1>Notes</h1>
+      <button className="Add-note" onClick={addNote}>+</button>
+      <Notes notes={notes}/>
     </div>
   )
 }
--- lab4/react-notes/src/components/Notes.tsx
+++ lab4/react-notes/src/components/Notes.tsx
@@ -1,30 +1,15 @@
 import "./Notes.css";
-import { useState } from "react";
 import Note from "./Note";
 
-type Note = {
-  id: number;
-  title: string;
-  body: string;
-};
+type NoteType = {
+  id: number,
+  title: string,
+  body: string
+}
 
-function Notes() {
-
-  const [notes, setNotes] = useState<Note[]>([]);
-
-  const addNote = () => {
-    const newNote = {
-      id: notes.length + 1,
-      title: "",
-      body: "",
-    };
-    setNotes([...notes, newNote]);
-  };
-
+function Notes({ notes }: { notes: NoteType[] }) {
   return (
     <div className="Notes">
-      <h1>Notes</h1>
-      <button className="Add-note" onClick={addNote}>+</button>
       <div className="Notes-list">
         {
           notes.map(note => (
