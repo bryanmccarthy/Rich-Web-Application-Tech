34dc9d9fb63fb12121f492db44a7eaead800e2a3
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-09 21:50:27 +0000

    make note editable

--- lab4/react-notes/src/components/Note.tsx
+++ lab4/react-notes/src/components/Note.tsx
@@ -3,8 +3,8 @@ import "./Note.css";
 function Note({ id, title, body, handleDelete }: { id: number, title: string, body: string, handleDelete: (params: any) => any }) {
   return (
     <div className="Note">
-      <h3>{title}</h3>
-      <p>{body}</p>
+      <h3 contentEditable suppressContentEditableWarning>{title}</h3>
+      <p contentEditable suppressContentEditableWarning>{body}</p>
       <button className="Delete-note" onClick={() => handleDelete(id)}>X</button>
     </div>
   );
